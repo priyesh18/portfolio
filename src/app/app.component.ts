b27a2fa339cb40f075efaf1a251a2fc3fd15340b
Priyesh Patel <safal.priyesh@gmail.com>
2018-06-18 21:41:06 +0530

    move contacts info and seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { BlogPage } from './../pages/blog/blog';
-import { ContactPage } from './../pages/contact/contact';
+//import { ContactPage } from './../pages/contact/contact';
 import { ExperiencePage } from './../pages/experience/experience';
 import { EducationPage } from './../pages/education/education';
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnDestroy } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,24 +13,24 @@ import { SkillsPage } from '../pages/skills/skills';
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
+export class MyApp implements OnDestroy {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, component: any, icon: string}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'About', component: HomePage },
-      { title: 'Skills', component: SkillsPage},
-      { title: 'Education', component: EducationPage},
-      { title: 'Experience', component: ExperiencePage},
-      { title: 'Blog', component: BlogPage },      
-      { title: 'Contact', component: ContactPage},
+      { title: 'About', component: HomePage, icon: 'information-circle' },
+      { title: 'Skills', component: SkillsPage, icon: 'code-working'},
+      { title: 'Education', component: EducationPage, icon: 'school'},
+      { title: 'Experience', component: ExperiencePage, icon: 'briefcase'},
+      { title: 'Blog', component: BlogPage, icon: 'book' },      
+      //{ title: 'Contact', component: ContactPage, icon: 'contact'},
     ];
 
   }
@@ -48,5 +48,16 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+  ionViewWillLeave() {
+    console.log('leaving');
+  }
+  ionViewCanLeave() : boolean {
+    let flag = true;
+    console.log(flag);
+    return false;
+  }
+  ngOnDestroy() {
+    console.log('destroyed');
   }
 }
