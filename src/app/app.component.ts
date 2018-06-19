de4e856d776f67789e9139b8d76f8a610414a1c0
Priyesh Patel <safal.priyesh@gmail.com>
2018-06-19 21:45:34 +0530

    add navigation back and change to dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { BlogPage } from './../pages/blog/blog';
 import { ExperiencePage } from './../pages/experience/experience';
 import { EducationPage } from './../pages/education/education';
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, App, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -14,14 +14,20 @@ import { SkillsPage } from '../pages/skills/skills';
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-
+  index = 1;
   rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any, icon: string}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar, 
+    public splashScreen: SplashScreen, 
+    private _app: App,
+    private _menu: MenuController
+  ) {
     this.initializeApp();
-
+    this.setupBackButtonBehavior ();
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'About', component: HomePage, icon: 'information-circle' },
@@ -43,7 +49,48 @@ export class MyApp {
   }
 
   openPage(page) {
-    this.nav.setRoot(page.component);
+    this._app.getRootNav().push(page.component);
   }
+  nextPage() {
+    if(this.index == this.pages.length) this.nav.popToRoot();
+    else {
+      this._app.getRootNav().push(this.pages[this.index].component)
+      //this.nav.push(this.pages[this.index].component);
+      this.index = (this.index+1);
+      
+    }
+  }
+
+  private setupBackButtonBehavior () {
+
+    // If on web version (browser)
+    if (window.location.protocol !== "file:") {
+
+      // Register browser back button action(s)
+      window.onpopstate = (evt) => {
+
+        // Close menu if open
+        if (this._menu.isOpen()) {
+          this._menu.close ();
+          return;
+        }
+
+        // Close any active modals or overlays
+        
+
+        // Navigate back
+        if (this._app.getRootNav().canGoBack()) this._app.getRootNav().pop();
+
+      };
+
+      // Fake browser history on each view enter
+      this._app.viewDidEnter.subscribe((app) => {
+        history.pushState (null, null, "");
+      });
+
+    }
+    
+  }
+
   
 }
