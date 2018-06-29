f18b739d7a92b6cbeff0bd73e9fded08a7128046
Priyesh Patel <safal.priyesh@gmail.com>
2018-06-30 00:25:03 +0530

    add skills data

--- src/pages/skills/skills.ts
+++ src/pages/skills/skills.ts
@@ -7,7 +7,7 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SkillsPage {
   coding = ['JavaScript','C/C++','Java(Basics)','HTML5 & CSS3','SQL'];
-  dev = ['Angular 2+', 'Ionic 2+', 'Node.js', 'Bootstrap', 'SEO'];
+  dev = ['Angular 2+', 'Ionic 2+', 'Node.js', 'Bootstrap', 'MongoDB(Basic)'];
   soft = ['Time Management', 'Self Motivation', 'Creativity', 'Team Work'];
   others = ['Hybrid Apps', 'SEO', 'PWA', 'Problem Solving']; //TODO
 
